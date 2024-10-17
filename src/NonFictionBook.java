5e75c98e95c3e155839c404c4943313d767a51c8
nicholassamuelssonjeria <nicholasjobb.jeria@gmail.com>
2024-10-17 14:56:31 +0200

    added subclasses

---
+++ src/NonFictionBook.java
@@ -0,0 +1,9 @@
+public class NonFictionBook extends Book {
+
+    String subject;
+    String genre;
+
+    public NonFictionBook(String title, String author, String pages, String subject, String genre) {
+        super(title, author, pages);
+    }
+}
