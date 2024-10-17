dc8d11a15d4298867a8ac651b63bd4ad219bbdf9
robinjakobsson <3ggapp24.jakoro@folkuniversitetet.nu>
2024-10-17 14:06:14 +0200

    gör om gör rätt

--- src/Library.java
+++ src/Library.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+import java.awt.print.Book;
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * Class to represent the Library
+ */
 public class Library {
+    protected List<Book> bookList = new ArrayList<>();
+    int index = 0;
+
+    /**
+     * Method to add a book
+     * @param book is the book
+     */
+    public void addBook (Book book){
+        bookList.add(book);
+    }
+
+    /**
+     * Method to show all books
+     */
+    public void showAllBooks(){
+        if (bookList == null){
+            System.out.println("No books to show :(");
+
+            for (Book b : bookList){
+                index++;
+                System.out.println(index + ". " + b.toString());
+            }
+        }
+    }
 }
